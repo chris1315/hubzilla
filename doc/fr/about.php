47af59f606fc0dd1b813d659fee301da7c8f421f
chris1315 <aktos@gmail.com>
2016-01-01 17:28:15 +0100

    Create about.php

---
+++ doc/fr/about.php
@@ -0,0 +1,5 @@
+[b]A propos[/b]
+
+$Projectname est un réseau de communication décentralisé, qui a pour but de proposer un moyen de communication qui est résistant à la censure, respecte la confidentialité et indépendant des grandes companies mondiales. Ces companies géantes fonctionnent comme des espions. Ce sont des monopoles centralisés sur internet. A l'orignie internet ne fonctionnait pas ainsi, il était décentralisé.
+
+$Projectname est libre et open source.  Il est conçu pour pouvoir monter en charge depuis un petite Raspberry Pi jusqu'à un serveur puissant à plusieurs coeurs. Il peut être utilisé pour servir de moyen de communication entre quelques individus mais peut monter pour héberger plusieurs milliers d'utilisateurs.
